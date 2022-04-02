ca1fdb25ed148a906fbfb1734482ba0fde8521c3
Marcin Polak <mapoart@gmail.com>
2022-04-02 11:10:05 +0200

    setup workflow

--- test_macos.js
+++ test_macos.js
@@ -6,6 +6,22 @@ console.log(os.platform()) // "win32"
 
 const { execSync } = require('child_process')
 
-const r = execSync('sw_vers').toString().trim()
+const releaseData = execSync('sw_vers').toString().trim()
 
-console.log('result macos: ', r)
+console.log('result macos: ', releaseData)
+
+const splitted = releaseData.split(/\r?\n/)
+const parsed = splitted
+  .map((e) => {
+    var s = e.split(':')
+    return { key: [s[0]], value: s[1] }
+  })
+  .reduce(
+    (acc, e) => ({
+      ...acc,
+      [e.key]: e.value && e.value.replace(/(^")|("$)/g, ''),
+    }),
+    {}
+  )
+
+console.log('parsed!!!!', parsed)
