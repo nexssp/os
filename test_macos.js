0451cde771c038653830bdd1151317e65c345c90
Marcin Polak <mapoart@gmail.com>
2022-04-02 10:57:49 +0200

    setup workflow

--- test_macos.js
+++ test_macos.js
@@ -1,5 +1,11 @@
+var os = require('os')
+
+console.log(os.type()) // "Windows_NT"
+console.log(os.release()) // "10.0.14393"
+console.log(os.platform()) // "win32"
+
 const { execSync } = require('child_process')
 
-const r = execSync('system_profiler').toString().trim()
+const r = execSync('sw_vers').toString().trim()
 
 console.log('result macos: ', r)
